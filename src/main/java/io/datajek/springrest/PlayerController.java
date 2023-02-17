99f170ed475028938b01283f7524fea4b1111768
OliveSonDev <olivesondev@gmail.com>
2023-02-17 09:11:00 +0900

    update: welcome get api

---
+++ src/main/java/io/datajek/springrest/PlayerController.java
@@ -0,0 +1,12 @@
+package io.datajek.springrest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PlayerController {
+    @GetMapping("/welcome")
+    public String welcome() {
+        return "Tennis Player REST API";
+    }
+}
