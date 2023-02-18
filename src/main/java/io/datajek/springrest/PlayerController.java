d7c8764db4f56155cc315de999fb3656273c809b
OliveSonDev <olivesondev@gmail.com>
2023-02-18 13:10:52 +0900

    update: return player per id get method

--- src/main/java/io/datajek/springrest/PlayerController.java
+++ src/main/java/io/datajek/springrest/PlayerController.java
@@ -2,6 +2,7 @@ package io.datajek.springrest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -19,6 +20,11 @@ public class PlayerController {
     @GetMapping("/players")
     public List<Player> allPlayers() {
         return service.getAllPlayers();
+    }
+
+    @GetMapping("/players/{id}")
+    public Player getPlayer(@PathVariable int id){
+        return service.getPlayer(id);
     }
 
 }
--- src/main/java/io/datajek/springrest/PlayerService.java
+++ src/main/java/io/datajek/springrest/PlayerService.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class PlayerService {
@@ -17,6 +18,17 @@ public class PlayerService {
     }
 
     //Get player by ID
+    public Player getPlayer(int id) {
+
+        Optional<Player> tempPlayer = repo.findById(id);
+        Player p = null;
+
+        //if the Optional has a value, assign it to p
+        if(tempPlayer.isPresent())
+            p = tempPlayer.get();
+
+        return p;
+    }
 
     //Add a player
 
